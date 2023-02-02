e3310b4627d454286acb0f441e787bf6ff55c71d
Uday Kumar Veeradasari <124153555+UdayKumar65@users.noreply.github.com>
2023-02-02 18:46:19 +0530

    Add files via upload
    Cuvette Task

---
+++ Cuvette-Task/bar.js
@@ -0,0 +1,29 @@
+google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],
+        ["xyz", 69, "#b87333"],
+        ["abc", 51, "silver"],
+        ["hero", 100, "gold"],
+        ["itachi", 80, "color: #e5e4e2"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Completion_rate",
+        width: 400,
+        height: 400,
+        bar: {groupWidth: "50%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.BarChart(document.getElementById("barchart_values"));
+      chart.draw(view, options);
+  }
---
+++ Cuvette-Task/chart.js
@@ -0,0 +1,29 @@
+google.charts.load("current", {packages:['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Element", "Density", { role: "style" } ],
+    ["uday", 10, "#b87333"],
+    ["kareem", 15, "silver"],
+    ["narasimha", 10.5, "gold"],
+    ["radhika", 21.45, "color: #e5e4e2"]
+  ]);
+
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0, 1,
+                   { calc: "stringify",
+                     sourceColumn: 1,
+                     type: "string",
+                     role: "annotation" },
+                   2]);
+
+  var options = {
+    title: "Unique_Plays",
+    width: 400,
+    height: 400,
+    bar: {groupWidth: "50%"},
+    legend: { position: "none" },
+  };
+  var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+  chart.draw(view, options);
+}
